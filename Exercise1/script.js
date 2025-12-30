da038580aff26ab8a4fb3351cd7a6e9539159201
Iman-Abdi <leofaragaab1@gmail.com>
2025-12-30 12:06:20 +0300

    Submit exercise 1 and Exercise 2

---
+++ Exercise1/script.js
@@ -0,0 +1,10 @@
+let x = 7;
+let y = 3;
+
+console.log(x + y); // Addition
+console.log(x - y); // Subtraction
+console.log(x * y); // Multiplication
+console.log(x / y); // Division
+console.log(x % y); // Modulus 
+console.log(++x);    // Increment
+console.log(--y);    // Decrement
