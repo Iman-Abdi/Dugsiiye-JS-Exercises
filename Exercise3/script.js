6cadb3505d48789a53682b84e52f512699a67ef3
Iman-Abdi <leofaragaab1@gmail.com>
2025-12-30 20:00:17 +0300

    Submited Exercise 3

---
+++ Exercise3/script.js
@@ -0,0 +1,10 @@
+let IsSunny = true;
+let IsWeakend = false;
+
+// And
+console.log(IsSunny && IsWeakend);
+// Or
+console.log(IsSunny || IsWeakend);
+// invert
+console.log(!IsSunny);
+console.log(!IsWeakend);
