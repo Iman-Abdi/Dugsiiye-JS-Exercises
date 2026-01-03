1f2046dc2558e4d65d2d4dbdbd5f7697e36f492b
Iman-Abdi <leofaragaab1@gmail.com>
2026-01-03 13:36:11 +0300

    Submiting JS Exercise 4

---
+++ Exercise4/main.js
@@ -0,0 +1,18 @@
+// function declaration
+
+function add(a, b) {
+    console.log(a+b);
+}
+
+add(5, 10);
+add(20, 30);
+
+
+// function expression
+
+const adding = function(a, b) {
+    return a + b;
+};
+
+console.log(adding(15, 25));
+console.log(adding(100, 200));
