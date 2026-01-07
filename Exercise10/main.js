8eafeb6a4ec78b65f6d090162feacb12c46888d6
Iman-Abdi <leofaragaab1@gmail.com>
2026-01-07 14:45:32 +0300

    Submitting Exercise 10

---
+++ Exercise10/main.js
@@ -0,0 +1,15 @@
+let Library = [
+    {
+        title : "The Great Gatsby",
+        Author : "F. Scott Fitzgerald",
+        Year : 1925,
+    },
+    {
+        title : "To Kill a Mockingbird",
+        Author : "Harper Lee",
+        Year : 1960,
+    }
+]
+
+console.log(Library[0].title);
+console.log(Library[1].Author);
