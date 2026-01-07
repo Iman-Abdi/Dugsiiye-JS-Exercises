4ac503df238e644a1d283cf059828fbebfe06e5e
Iman-Abdi <leofaragaab1@gmail.com>
2026-01-07 14:09:10 +0300

    Submitting Exercise 7

---
+++ Exercise7/main.js
@@ -0,0 +1,18 @@
+let Colors = ["Blue", "Red", "Green"];
+
+console.log("Before");
+
+console.log(Colors);
+
+console.log("After Push");
+
+Colors.push("Orange");
+
+console.log(Colors);
+
+console.log("After shift");
+
+Colors.shift();
+
+console.log(Colors);
+
