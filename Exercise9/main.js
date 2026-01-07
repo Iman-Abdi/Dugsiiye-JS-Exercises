28ca1759f8e1a619846901f20e0d78e009f2b1de
Iman-Abdi <leofaragaab1@gmail.com>
2026-01-07 14:34:35 +0300

    Submitting Exercise 9

---
+++ Exercise9/main.js
@@ -0,0 +1,12 @@
+let Car = {
+    Make : "Toyota",
+    Model : "Corolla",
+    Year : 2020,
+
+    Start : function() {
+        console.log("The Car has Started");
+    }
+}
+
+
+console.log(Car.Start());
