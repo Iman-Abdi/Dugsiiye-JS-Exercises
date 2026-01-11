b6978bc2ace63ed8f78919e7a03bf218c8873f40
Iman-Abdi <leofaragaab1@gmail.com>
2026-01-11 14:47:32 +0300

    Exercise 17

---
+++ Exercise17/main.js
@@ -0,0 +1,13 @@
+let Temoerature = 22;
+
+if (Temoerature < 0) {
+  console.log("Very Cold")
+}else if (Temoerature < 15 ) {
+  console.log("Cold")
+}
+else if (Temoerature < 25 ) {
+  console.log("Warm")
+}
+else {
+  console.log("Hot")
+}
